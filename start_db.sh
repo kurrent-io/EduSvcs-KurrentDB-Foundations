e7e4a900df47fc5ac7b2151403763a0cb35e7a7d
Paulo Borges <paulohtb@hotmail.com>
2025-04-30 12:33:50 -0300

    Update start_db.sh

--- start_db.sh
+++ start_db.sh
@@ -30,7 +30,7 @@
 
 
 ###########################################################
-# Set EventStoreDB version
+# Set KurrentDB version
 # Version is set to whatever the LTS version is at
 # the time this script was last tested
 # If you wanted to manually change (at your own risk)
@@ -38,7 +38,7 @@
 #
 ###########################################################
 
-db_version=eventstore/eventstore:24.10.1
+db_version=kurrent-latest/kurrentdb:latest
 
 #################################################################################
 # Some bash notes for the curious
@@ -47,7 +47,7 @@ db_version=eventstore/eventstore:24.10.1
 # echo -e the -e allows echo of newline characters
 #################################################################################
 
-echo "Starting EventStoreDB docker container. This can take a moment..."
+echo "Starting KurrentDB docker container. This can take a moment..."
 
 ##################################################
 #
@@ -68,33 +68,33 @@ done
 
 #####################################################################################
 #
-# Step 2. Stop and remove the EventStoreDB docker container if it is already running
+# Step 2. Stop and remove the KurrentDB docker container if it is already running
 #
 #####################################################################################
 
-if docker inspect esdb-node > /dev/null 2>&1; then                            # If the EventStoreDB docker container (esdb-node) is running
-       echo -e 'EventStoreDB docker container appears to be running...';
-       echo -e 'Stopping and removing the EventStoreDB docker container...';
+if docker inspect esdb-node > /dev/null 2>&1; then                            # If the KurrentDB docker container (esdb-node) is running
+       echo -e 'KurrentDB docker container appears to be running...';
+       echo -e 'Stopping and removing the KurrentDB docker container...';
 
        docker rm -f esdb-node > /dev/null 2>&1                                # Remove the docker container
 fi
 
 ################################################################################################################################################
 #
-# Step 3. Run the EventStoreDB docker container
+# Step 3. Run the KurrentDB docker container
 #
 # docker run                        # Start a new Docker container using the 'docker run' command
 #      -d \                         # Run the container in detached mode (in the background)
 #      --name esdb-node \           # Assign the container a name ('esdb-node' in this case)
-#      -p 2113:2113 \               # Map port 2113 on the host to port 2113 in the docker container. Required for EventStoreDB
-#      eventstore/eventstore:lts \  # Specify the Docker image to use, in this case, the EventStoreDB long-term support version (lts)
-#      --insecure \                 # Run EventStoreDB in insecure mode, without authentication and SSL/TLS security (usually for development)
-#      --run-projections=All \      # Enable all projections in EventStoreDB, including system and user projections
-#      --enable-atom-pub-over-http  # Enable the AtomPub API over HTTP. Required for the EventStoreDB Admin UI
+#      -p 2113:2113 \               # Map port 2113 on the host to port 2113 in the docker container. Required for KurrentDB
+#      eventstore/eventstore:lts \  # Specify the Docker image to use, in this case, the KurrentDB long-term support version (lts)
+#      --insecure \                 # Run KurrentDB in insecure mode, without authentication and SSL/TLS security (usually for development)
+#      --run-projections=All \      # Enable all projections in KurrentDB, including system and user projections
+#      --enable-atom-pub-over-http  # Enable the AtomPub API over HTTP. Required for the KurrentDB Admin UI
 #
 ################################################################################################################################################
 
-echo -e 'Starting the EventStoreDB docker container...';
+echo -e 'Starting the KurrentDB docker container...';
 
 docker run \
        -d \
@@ -106,26 +106,26 @@ docker run \
        --enable-atom-pub-over-http \
        --start-standard-projections
 
-if ! docker inspect esdb-node > /dev/null 2>&1; then    # If the EventStoreDB docker container (esdb) is running, exit
-       echo "The EventStoreDB docker container is not running. Exiting."
+if ! docker inspect esdb-node > /dev/null 2>&1; then    # If the KurrentDB docker container (esdb) is running, exit
+       echo "The KurrentDB docker container is not running. Exiting."
        exit 1
 fi
 
 ######################################################################################
 #
-# Step 4. Print success message if EventStoreDB docker container started successfully
+# Step 4. Print success message if KurrentDB docker container started successfully
 #
 ######################################################################################
 
-ESDB_URL=http://localhost:2113                                                            # Set default URL to localhost (for EventStoreDB started locally, not in Codespaces)
+ESDB_URL=http://localhost:2113                                                            # Set default URL to localhost (for KurrentDB started locally, not in Codespaces)
 if [ "$CODESPACES" == "true" ]; then                                                      # If this environment is Codespaces 
        ESDB_URL=https://"$CODESPACE_NAME"-2113.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN  # Build the URL to forwarded github codespaces domain       
 fi
 
 echo ""
 echo ""
-echo -e "🚀 \e[32mEventStoreDB Server has started!!\e[0m 🚀" 
+echo -e "🚀 \e[32mKurrentDB Server has started!!\e[0m 🚀" 
 echo ""
-echo -e "URL to EventStoreDB Admin UI 👉 \e[0m \e[34m$ESDB_URL\e[0m"                      # Print URL to EventStoreDB Admin UI
+echo -e "URL to KurrentDB Admin UI 👉 \e[0m \e[34m$ESDB_URL\e[0m"                      # Print URL to KurrentDB Admin UI
 echo ""
 echo ""
